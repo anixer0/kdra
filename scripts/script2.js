c2688c3f3bfded2ba8a5403f02f26dcb642606d6
Anixer <anixer42@gmail.com>
2024-11-19 10:32:18 +0800

    Update script2.js

--- scripts/script2.js
+++ scripts/script2.js
@@ -19,7 +19,7 @@ test();
 
 let z = '';
 async function test() {
-	const infoUrl = `https://msks.vercel.app/movies/dramacool/info?id=${seriesID}`;
+	const infoUrl = `https://kd1-puce.vercel.app/movies/dramacool/info?id=${seriesID}`;
 	const { data: infoData } = await axios.get(infoUrl);
 
 	if (infoData.episodes && infoData.episodes.length > 0) { // getting number of episodes and their respective links
@@ -45,7 +45,7 @@ document.addEventListener("click", async function (event) {
 		const button = event.target;
 		epiID = button.getAttribute("data-episode-id");
 	
-		const watchUrl = `https://msks.vercel.app/movies/dramacool/watch?episodeId=${epiID}&mediaId=${seriesID}`;
+		const watchUrl = `https://kd1-puce.vercel.app/movies/dramacool/watch?episodeId=${epiID}&mediaId=${seriesID}`;
 		try {
 
 			const { data: Links } = await axios.get(watchUrl);
